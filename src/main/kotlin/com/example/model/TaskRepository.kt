803b501b4d8ca3c7ebb2e7876a8a790e8d987893
TaewoongR <iwoong98@kau.kr>
2024-07-10 12:33:28 +0900

    GET route

---
+++ src/main/kotlin/com/example/model/TaskRepository.kt
@@ -0,0 +1,27 @@
+package com.example.model
+
+object TaskRepository {
+    private val tasks = mutableListOf(
+        Task("cleaning", "Clean the house", Priority.Low),
+        Task("gardening", "Mow the lawn", Priority.Medium),
+        Task("shopping", "Buy the groceries", Priority.High),
+        Task("painting", "Paint the fence", Priority.Medium)
+    )
+
+    fun allTasks(): List<Task> = tasks
+
+    fun tasksByPriority(priority: Priority) = tasks.filter{
+        it.priority == priority
+    }
+
+    fun tasksByName(name: String) = tasks.find{
+        it.name.equals(name, ignoreCase = true)
+    }
+
+    fun addTask(task: Task){
+        if(tasksByName(task.name) != null){
+            throw IllegalStateException("cannot duplicate task names!")
+        }
+        tasks.add(task)
+    }
+}
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.model.*
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
@@ -10,15 +11,44 @@ fun Application.configureRouting() {
     routing {
         staticResources("static", "static")
 
-        get("/tasks") {
-            call.respond(
-                listOf(
-                    Task("cleaning", "Clean the house", Priority.Low),
-                    Task("gardening", "Mow the lawn", Priority.Medium),
-                    Task("shopping", "Buy the groceries", Priority.High),
-                    Task("painting", "Paint the fence", Priority.Medium)
-                )
-            )
+        route("/tasks") {
+            get {
+                val tasks = TaskRepository.allTasks()
+                call.respond(tasks)
+            }
+
+            get("/byName/{taskName}"){
+                val name = call.parameters["taskName"]
+                if(name == null){
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+                val task = TaskRepository.tasksByName(name)
+                if(task == null){
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+                call.respond(task)
+            }
+
+            get("/byPriority/{priority}"){
+                val priorityAsText = call.parameters["priority"]
+                if(priorityAsText == null){
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+                try{
+                    val priority = Priority.valueOf(priorityAsText)
+                    val tasks = TaskRepository.tasksByPriority(priority)
+                    if(tasks.isEmpty()){
+                        call.respond(HttpStatusCode.NotFound)
+                        return@get
+                    }
+                    call.respond(tasks)
+                }catch (e: IllegalArgumentException){
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
         }
     }
 }
